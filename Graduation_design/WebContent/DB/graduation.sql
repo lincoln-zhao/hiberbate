528830cde3670b327653a07ec70122298ef62138
lincoln-zhao <836602816@qq.com>
2017-05-08 22:45:36 +0800

    up DBsql

--- Graduation_design/WebContent/DB/graduation.sql
+++ Graduation_design/WebContent/DB/graduation.sql
@@ -4,7 +4,7 @@ Source Host: localhost
 Source Database: graduation
 Target Host: localhost
 Target Database: graduation
-Date: 2017/5/7 22:11:13
+Date: 2017/5/8 22:44:36
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -12,15 +12,15 @@ SET FOREIGN_KEY_CHECKS=0;
 -- Table structure for t_user
 -- ----------------------------
 CREATE TABLE `t_user` (
-  `USER_ID` int(5) NOT NULL AUTO_INCREMENT,
+  `USER_ID` int(10) NOT NULL AUTO_INCREMENT,
   `USER_NAME` varchar(20) NOT NULL,
-  `PASSWORD` varchar(20) NOT NULL,
-  `SEX` varchar(2) DEFAULT NULL,
-  `PHONE` int(11) DEFAULT NULL,
+  `PASSWORD` varchar(10) NOT NULL,
+  `SEX` varchar(5) DEFAULT NULL,
+  `PHONE` varchar(11) DEFAULT NULL,
   PRIMARY KEY (`USER_ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records 
 -- ----------------------------
-INSERT INTO `t_user` VALUES ('1', 'lincoln', '123', '1', '11');
+INSERT INTO `t_user` VALUES ('1', 'lincoln', '123', '男', '888');
