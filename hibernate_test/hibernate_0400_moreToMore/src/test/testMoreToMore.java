ed2612ac3dc32d83aab9f07ca0ff14f773519d50
lincoln-zhao <836602816@qq.com>
2017-06-20 22:23:53 +0800

    多对多添加

--- hibernate_test/hibernate_0400_moreToMore/src/test/testMoreToMore.java
+++ hibernate_test/hibernate_0400_moreToMore/src/test/testMoreToMore.java
@@ -3,6 +3,8 @@ package test;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
+import com.lincoln.entity.Role;
+import com.lincoln.entity.User;
 import com.lincoln.util.GetSessionFactory;
 
 public class testMoreToMore {
@@ -15,7 +17,32 @@ public class testMoreToMore {
 		session.getTransaction().begin();
 		
 		
+		User u1 = new User();
+		u1.setUser_name("lincoln");
+		u1.setPassword("123");
 		
+		User u2 = new User();
+		u2.setUser_name("mary");
+		u2.setPassword("456");
+		
+		Role r1 = new Role();
+		r1.setRole_name("主人");
+		
+		Role r2 = new Role();
+		r2.setRole_name("女仆");
+		
+		Role r3 = new Role();
+		r3.setRole_name("护士");
+		
+		u1.getRoleSet().add(r1);
+		u1.getRoleSet().add(r2);
+		
+		u2.getRoleSet().add(r2);
+		u2.getRoleSet().add(r3);
+		
+		
+		session.save(u1);
+		session.save(u2);
 		
 		
 		
